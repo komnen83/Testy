950c11385ef82051c7a5c96ebb591867ecf281af
komnen <kuba.bauman@gmail.com>
2020-02-22 13:56:48 +0100

    zad.6

---
+++ src/test/java/testy/InsertionSortTest.java
@@ -0,0 +1,22 @@
+package testy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InsertionSortTest {
+
+    @Test
+    void insertionSortImperative() {
+
+        int[] input = {6, 2, 3, 4, 5, 1};
+        
+        int[] expected = {1, 2, 3, 4, 5, 6};
+        int[] result = InsertionSort.insertionSortImperative(input);
+
+        Assertions.assertEquals(Arrays.toString(expected), Arrays.toString(result), "the two arrays are not equal");
+    }
+}
